e34435bfea99f6b1a1a7ba5e2132a784d39999fe
Luis-AP <lap18958@gmail.com>
2024-11-22 20:55:36 -0300

    Esquema URL de DRF

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import welcome, get_autors, get_autors_as_json
+from rest_framework.routers import DefaultRouter
+from .viewsets import AutorViewSet
 
+router = DefaultRouter()
+
+router.register(r"autors", AutorViewSet)
 
 urlpatterns = [
     path("welcome", welcome),
     path("autores", get_autors),
     path("autores/json", get_autors_as_json),
+    path("", include(router.urls)),
 ]
