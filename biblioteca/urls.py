f5584fe793f652fd93edae45e94681686c5c8b65
Luis-AP <lap18958@gmail.com>
2024-11-22 20:43:08 -0300

    Vistas, respuesta como HTML y JSON

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import welcome
+from .views import welcome, get_autors, get_autors_as_json
 
-urlpatterns = [path("welcome", welcome)]
+
+urlpatterns = [
+    path("welcome", welcome),
+    path("autores", get_autors),
+    path("autores/json", get_autors_as_json),
+]
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import datetime
+from .models import Autor
 
 
 def welcome(request):
@@ -7,3 +8,42 @@ def welcome(request):
     return HttpResponse(
         f"Bienvenido a la biblioteca 'Mar de tinta'. La hora del servidor es {now.strftime('%A, %d %B, %Y a las %X')}."
     )
+
+
+def get_autors(request):
+    """
+    Devolver todos los autores con un formato HTML
+    """
+    # Obtener todos los autores
+    autores = Autor.objects.all()
+
+    # Crear una respuesta con los autores
+    response = "<h1>Autores</h1>"
+    for autor in autores:
+        response += f"<h3>{autor.nombre}</h3>"
+        response += f"<p>{autor.bio}</p>"
+        response += f"<p>Creado en: {autor.creado_en} - Actualizado en: {autor.actualizado_en}</p>"
+
+    return HttpResponse(response)
+
+
+def get_autors_as_json(request):
+    """
+    Devolver todos los autores con un formato JSON
+    """
+    # Obtener todos los autores
+    autores = Autor.objects.all()
+
+    # Crear un diccionario con los autores para serializarlo a JSON
+    response = {"autores": []}
+    for autor in autores:
+        response["autores"].append(
+            {
+                "nombre": autor.nombre,
+                "bio": autor.bio,
+                "creado_en": autor.creado_en,
+                "actualizado_en": autor.actualizado_en,
+            }
+        )
+
+    return JsonResponse(response)
